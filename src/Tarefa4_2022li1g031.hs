09e60078c362f44559d9a720885a7e8aa1dc0405
Darguima <dsgdevbraga@gmail.com>
2022-11-13 23:48:07 +0000

    Tarefa 4

--- src/Tarefa4_2022li1g031.hs
+++ src/Tarefa4_2022li1g031.hs
@@ -10,5 +10,30 @@ module Tarefa4_2022li1g031 where
 
 import LI12223
 
+{- | A função jogoTerminou indica se o jogador perdeu o jogo, onde True significa que sim. Para isso
+ela testa se o jogador se encontra fora do mapa, na água, ou “debaixo”
+de um carro (i.e. na mesma posição de um carro.)
+
+Para tal foram criadas duas funções auxiliares: `auxObstaculos` e `auxPosicao` para analisar se está a colidir com um obstaculos e se ainda 
+está dentro do mapa respetivamente.
+
+== Exemplos de utilização:
+>>> jogoTerminou (Jogo (Jogador (0,0))  (Mapa 4 [ (Rio 5 , [Tronco, Tronco, Tronco, Nenhum] )]))
+False
+
+>>> jogoTerminou (Jogo (Jogador (0,0))  (Mapa 4 [ (Estrada 5 , [Carro, Carro, Nenhum, Nenhum] )]))
+True
+
+-}
+
 jogoTerminou :: Jogo -> Bool
-jogoTerminou = undefined
+jogoTerminou (Jogo (Jogador (x,y)) (Mapa largura linhasMapa)) = auxPosicao largura x || auxObstaculos terrenoLinhaAtual (obstaculosLinhaAtual !! x)
+  where (terrenoLinhaAtual, obstaculosLinhaAtual) = linhasMapa !! y
+        auxObstaculos :: Terreno -> Obstaculo -> Bool
+        auxObstaculos (Rio _) Nenhum = True
+        auxObstaculos (Estrada _) Carro = True
+        auxObstaculos _ _ = False
+
+        auxPosicao :: Int -> Int -> Bool
+        auxPosicao largura posicaoX = largura <= posicaoX
+
--- tests/Tarefa4_2022li1g031_Spec.hs
+++ tests/Tarefa4_2022li1g031_Spec.hs
@@ -5,4 +5,23 @@ import Tarefa4_2022li1g031
 import Test.HUnit
 
 testsT4 :: Test
-testsT4 = TestLabel "Testes Tarefa 4" $ test ["Teste 1" ~: 1 ~=? 1]
+
+jogoTerminouTestes = TestList [
+  "Teste Jogo Terminou 01" ~: False ~=? jogoTerminou (Jogo (Jogador (0,0))  (Mapa 4 [ (Rio 5 , [Tronco, Tronco, Tronco, Nenhum]) ])),
+  "Teste Jogo Terminou 02" ~: True ~=? jogoTerminou (Jogo (Jogador (2,0))  (Mapa 4 [ (Rio 5 , [Tronco, Tronco, Nenhum, Nenhum]) ])),
+
+  "Teste Jogo Terminou 03" ~: False ~=? jogoTerminou (Jogo (Jogador (0,0))  (Mapa 4 [ (Estrada 5 , [Nenhum, Nenhum, Carro, Carro]) ])),
+  "Teste Jogo Terminou 04" ~: True ~=? jogoTerminou (Jogo (Jogador (0,0))  (Mapa 4 [ (Estrada 5 , [Carro, Carro, Nenhum, Nenhum]) ])),
+
+  "Teste Jogo Terminou 05" ~: False ~=? jogoTerminou (Jogo (Jogador (2,0))  (Mapa 4 [ (Relva , [Arvore, Arvore, Nenhum, Nenhum]) ])),
+
+  "Teste Jogo Terminou 06" ~: False ~=? jogoTerminou (Jogo (Jogador (2,0))  (Mapa 4 [ (Estrada 5 , [Carro, Carro, Nenhum, Nenhum]) ])),
+  "Teste Jogo Terminou 07" ~: True ~=? jogoTerminou (Jogo (Jogador (4,0))  (Mapa 4 [ (Relva , [Nenhum, Nenhum, Arvore, Arvore]) ])),
+
+  "Teste Jogo Terminou 08" ~: False ~=? jogoTerminou (Jogo (Jogador (0, 1))  (Mapa 4 [ (Relva , [Nenhum, Nenhum, Arvore, Arvore]), (Estrada 5 , [Nenhum, Carro, Carro, Nenhum]), (Rio 3 , [Nenhum, Tronco, Tronco, Tronco]) ])),
+  "Teste Jogo Terminou 09" ~: True ~=? jogoTerminou (Jogo (Jogador (0 ,2))  (Mapa 4 [ (Relva , [Nenhum, Nenhum, Arvore, Arvore]), (Estrada 5 , [Nenhum, Carro, Carro, Nenhum]), (Rio 3 , [Nenhum, Tronco, Tronco, Tronco]) ])),
+  "Teste Jogo Terminou 10" ~: True ~=? jogoTerminou (Jogo (Jogador (4, 2))  (Mapa 4 [ (Relva , [Nenhum, Nenhum, Arvore, Arvore]), (Estrada 5 , [Nenhum, Carro, Carro, Nenhum]), (Rio 3 , [Nenhum, Tronco, Tronco, Tronco]) ]))
+  ] 
+
+
+testsT4 = TestList [jogoTerminouTestes]
