031b34d7a6081a2561155a5fa39e6691b22cdbae
Darguima <dsgdevbraga@gmail.com>
2022-12-07 10:43:17 +0000

    Adicionado estado de derrota e jogo ao estado

--- src/Tarefa6_2022li1g031.hs
+++ src/Tarefa6_2022li1g031.hs
@@ -14,6 +14,7 @@ import Graphics.Gloss.Interface.IO.Game
 import Graphics.Gloss.Interface.Environment
 import Tarefa5_2022li1g031 (deslizaJogo)
 import Tarefa3_2022li1g031 (animaJogo)
+import Tarefa4_2022li1g031 (jogoTerminou)
 {-|
 O objetivo desta tarefa consiste em aproveitar todas as funcionalidades já
 elaboradas nas outras Tarefas e construir uma aplicação gráfica que permita
@@ -28,7 +29,9 @@ alunos, sendo que, no mı́nimo, a aplicação deverá:
 4. Detetar quando o jogador perde e reagir adequadamente, e.g mostrando uma mensagem, reiniciando o jogo, etc.
 -}
 
-type Estado = (Jogo, Texturas, Int)
+data MenuAtual = INICIAL | JOGO | DERROTA
+
+type Estado = (Jogo, Texturas, Int, MenuAtual)
 type Texturas = [Picture]
 
 tamanhoChunk :: Float
@@ -40,13 +43,19 @@ getInitialX larguraMapa = - (fromIntegral larguraMapa * tamanhoChunk) / 2 +  (ta
 getInitialY :: Int -> Float
 getInitialY alturaWindow = (fromIntegral alturaWindow / 2) - (tamanhoChunk / 2)
 
-mapaInicial = Mapa 8 [(Rio 5, [Nenhum, Tronco, Nenhum,Nenhum,Nenhum,Nenhum,Tronco,Tronco]), (Estrada 2 , [Nenhum,Carro,Nenhum,Carro,Carro,Nenhum,Carro,Carro]), (Estrada 2 , [Nenhum,Carro,Nenhum,Nenhum,Carro,Nenhum,Nenhum,Carro]), (Rio 2 , [Nenhum,Tronco,Nenhum,Tronco,Tronco,Nenhum,Nenhum,Nenhum]), (Relva , [Nenhum,Arvore,Nenhum,Arvore,Arvore,Nenhum,Nenhum,Nenhum]), (Estrada 2 , [Nenhum,Carro,Nenhum,Nenhum,Carro,Carro,Nenhum,Nenhum]), (Estrada 1, [Carro,Nenhum,Nenhum,Carro,Carro,Nenhum,Nenhum,Nenhum]), (Rio 1, [Tronco,Nenhum,Nenhum,Tronco,Tronco,Nenhum,Nenhum,Nenhum]),(Rio 2 , [Nenhum,Tronco,Nenhum,Tronco,Tronco,Nenhum,Nenhum,Nenhum]), (Relva , [Nenhum,Arvore,Nenhum,Arvore,Arvore,Nenhum,Nenhum,Nenhum]), (Estrada 2 , [Nenhum,Carro,Nenhum,Nenhum,Carro,Carro,Nenhum,Nenhum])] 
+mapaInicial = Mapa 8 [(Estrada 2 , [Nenhum,Carro,Nenhum,Nenhum,Carro,Carro,Nenhum,Nenhum]), (Estrada 1, [Carro,Nenhum,Nenhum,Carro,Carro,Nenhum,Nenhum,Nenhum]), (Rio 1, [Tronco,Nenhum,Nenhum,Tronco,Tronco,Nenhum,Nenhum,Nenhum]),(Rio 2 , [Nenhum,Tronco,Nenhum,Tronco,Tronco,Nenhum,Nenhum,Nenhum]), (Relva , [Nenhum,Arvore,Nenhum,Arvore,Arvore,Nenhum,Nenhum,Nenhum]), (Estrada 2 , [Nenhum,Carro,Nenhum,Nenhum,Carro,Carro,Nenhum,Nenhum]), (Relva , [Nenhum,Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum, Nenhum]), (Relva , [Nenhum,Nenhum,Arvore,Nenhum,Nenhum,Nenhum, Nenhum, Nenhum]), (Relva , [Nenhum,Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum, Nenhum]), (Relva , [Nenhum,Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum, Nenhum]), (Relva , [Nenhum,Nenhum,Arvore,Nenhum,Nenhum,Nenhum, Nenhum, Nenhum]), (Relva , [Nenhum,Arvore,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum,Nenhum])] 
+
+getPosInicial :: Coordenadas
+getPosInicial = getPosInicial_ mapaInicial
+  where getPosInicial_ :: Mapa -> (Int, Int)
+        getPosInicial_ (Mapa largura linhasMap@((_, linhaMapa) : _)) = (div (length linhaMapa) 2, length linhasMap - 4)
+
 
 estadoInicial :: Texturas -> Int -> Estado
-estadoInicial texturas alturaWindow = (Jogo (Jogador (0, 0)) mapaInicial, texturas, alturaWindow )
+estadoInicial texturas alturaWindow = (Jogo (Jogador getPosInicial) mapaInicial, texturas, alturaWindow, JOGO )
 
 desenharNovoEstado :: Estado -> IO Picture
-desenharNovoEstado (Jogo (Jogador (posX, posY)) mapa@(Mapa larguraMapa _), texturas, alturaWindow) = do
+desenharNovoEstado (Jogo (Jogador (posX, posY)) mapa@(Mapa larguraMapa _), texturas, alturaWindow, JOGO) = do
   return $ Pictures $ desenharMapa mapa texturas (getInitialX larguraMapa) (getInitialY alturaWindow) ++ [desenharPlayer posX posY larguraMapa alturaWindow]
 
   where desenharMapa :: Mapa -> Texturas -> Float -> Float -> [Picture]
@@ -75,13 +84,24 @@ desenharNovoEstado (Jogo (Jogador (posX, posY)) mapa@(Mapa larguraMapa _), textu
           where posX = getInitialX larguraMapa + fromIntegral posXJogador * tamanhoChunk
                 posY = getInitialY alturaWindow - fromIntegral posYJogador * tamanhoChunk
 
+desenharNovoEstado (jogo, texturas, alturaWindow, DERROTA) = return $ color red $ circle 20
+desenharNovoEstado (jogo, texturas, alturaWindow, _) = return $ color green $ circle 20
+
 reageEvento :: Event -> Estado -> IO Estado
 reageEvento _ = return
 
 reageTempo :: Float -> Estado -> IO Estado
-reageTempo _ (jogo, texturas, alturaWindow) = do
+
+reageTempo _ (jogo, texturas, alturaWindow, DERROTA) = do
+  return (jogo, texturas, alturaWindow, DERROTA)
+
+reageTempo _ (jogo, texturas, alturaWindow, JOGO) = do
   novoJogo <- deslizaJogo $ animaJogo jogo (Move Cima)
-  return (novoJogo, texturas, alturaWindow)
+  let novoMenu = if jogoTerminou novoJogo then DERROTA else JOGO
+  return (jogo, texturas, alturaWindow, novoMenu)
+
+reageTempo _ (jogo, texturas, alturaWindow, menuAtual) = do
+  return (jogo, texturas, alturaWindow, menuAtual)
 
 fr :: Int
 fr = 1
